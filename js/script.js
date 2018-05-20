58b3f6b9a41cf9bd69e0c7c8c81bd39775168224
Bhaskar Khaneja <bkhaneja3@gatech.edu>
2018-05-20 21:45:29 +0530

    updated typed text and added icons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-var typed = new Typed('.element', {
-    stringsElement: '#typed-strings'
-  });
+var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
+	startDelay: 10,
+	typeSpeed: 50,
+	backSpeed: 10,
+	backDelay: 2000,
+	smartBackspace: false,
+	loop: true,
+	//fadeOut: true
+});
